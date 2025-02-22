c4cea2b515a426a3a4216f6ff94f0185c4b5dd8d
khshayar <khshayar9605@gmail.com>
2025-02-22 10:52:23 +0330

    entitie creat

---
+++ Core/Entities/Personnels/Personnel.cs
@@ -0,0 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace Entities.Personnels;
+
+public class Personnel
+{
+    public int Id { get; set; }
+    [Required]
+    public string FirstName { get; set; }
+    public string LastName { get; set; }
+    [Required]
+    public string NationalCode { get; set; }
+    public string Email { get; set; }
+    public string PhoneNumber { get; set; }
+    public DateTime CreatAt { get; set; }
+}
